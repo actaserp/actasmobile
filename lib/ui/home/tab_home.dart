eee28b4af9287f9eb93c131ae2a502e791529666
gyeonghyeonlim <t_rror@naver.com>
2023-02-16 17:02:47 +0900

    home

--- lib/ui/home/tab_home.dart
+++ lib/ui/home/tab_home.dart
@@ -1,3 +1,4 @@
+import 'package:actasm/ui/home/app5Home/appPager13.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:actasm/config/constant.dart';
 import 'package:actasm/model/feature/banner_slider_model.dart';
@@ -9,12 +10,16 @@ import 'package:flutter/services.dart';
 import 'package:flutter_session_manager/flutter_session_manager.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-import 'app03/appPage03.dart';
 import 'app04/appPage04.dart';
 import 'app05/appPage05.dart';
+import 'app08/appPage08.dart';
+import 'app09/appPage09.dart';
+import 'app10/appPage10.dart';
 import 'app5Home/appPager10.dart';
-import 'app5Home/tab5_home.dart';
 import 'appPage02.dart';
+import 'app03/appPage03.dart';
+import 'app5Home/tab5_home.dart';
+
 
 class TabHomePage extends StatefulWidget {
   @override
@@ -69,7 +74,7 @@ class _Home1PageState extends State<TabHomePage> {
 
 
   Future<void> setData() async {
-    _usernm = await  SessionManager().get("username");
+      _usernm = await  SessionManager().get("username");
   }
 
   @override
@@ -238,7 +243,7 @@ class _Home1PageState extends State<TabHomePage> {
               String ls_name = _categoryData[index].name.replaceAll('\n', ' ');
               switch (ls_name){
                 case '고 장 접 수' :
-                // Navigator.push(context, MaterialPageRoute(builder: (context) => CouponPage()));
+                  // Navigator.push(context, MaterialPageRoute(builder: (context) => CouponPage()));
                   break;
                 case '고 장 처 리' :
                   Navigator.push(context, MaterialPageRoute(builder: (context) => AppPage02()));
@@ -248,10 +253,13 @@ class _Home1PageState extends State<TabHomePage> {
                 case '점 검 조 치' :
                   break;
                 case '도 면 자 료' :
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => AppPage08()));
                   break;
                 case '부 품 자 료' :
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => AppPage10()));
                   break;
                 case '기 타 자 료' :
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => AppPage09()));
                   break;
                 case '현장정보 승강기번호조회 비상통화조회' :
                   break;
@@ -260,9 +268,11 @@ class _Home1PageState extends State<TabHomePage> {
                   break;
                 case '부 품 가 이 드' :
                   Navigator.push(context, MaterialPageRoute(builder: (context) => AppPage04()));
+
                   break;
                 case '수 리 Q  &  A' :
                   Navigator.push(context, MaterialPageRoute(builder: (context) => AppPage05()));
+
                   break;
                 case '직 원 정 보' :
                   Navigator.push(context, MaterialPageRoute(builder: (context) => AppPager10()));
@@ -270,13 +280,12 @@ class _Home1PageState extends State<TabHomePage> {
                 case '고 장 이 력' :
                   break;
                 case '고 장 통 계' :
-                /*Navigator.push(context, MaterialPageRoute(builder: (context) => AppPage02()));
-                  */
                   Navigator.push(context, MaterialPageRoute(builder: (context) => Tab5HomePage()));
                   break;
                 case '작 업 일 보' :
                   break;
                 case '공 지 사 항' :
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => AppPager13()));
                   break;
                 default:
                   break;
@@ -286,7 +295,7 @@ class _Home1PageState extends State<TabHomePage> {
               decoration: BoxDecoration(
                   border: Border.all(color: Colors.grey[100]!, width: 0.5),
                   color: Colors.white),  //Colors.white
-              padding: EdgeInsets.all(8),
+                  padding: EdgeInsets.all(8),
               child: Center(
                   child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -309,7 +318,4 @@ class _Home1PageState extends State<TabHomePage> {
       }),
     );
   }
-
 }
-
-
