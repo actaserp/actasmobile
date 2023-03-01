04ca5330a6110a1037ed2d44e904af7b8e24944d
Lee-JuSeon <quasar.joo@gmail.com>
2023-03-01 14:56:02 +0900

    navigator 롤백

--- lib/ui/home/app03/Nav_right.dart
+++ lib/ui/home/app03/Nav_right.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../../model/app03/nav_model.dart';
 import '../appPage02.dart';
 import '../tab_home.dart';
 
@@ -13,7 +12,7 @@ class Nav_right extends StatelessWidget{
 
   Nav_right(
       {Key? key,
-  required this.text, required this.color,
+        required this.text, required this.color,
 
       }) : super(key: key);
 
@@ -23,43 +22,83 @@ class Nav_right extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Drawer(
-              child: ListView(
-              padding: EdgeInsets.zero,
-              children: <Widget>[
-                    DrawerHeader(child:
-                        Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text('Actas',
-                          style: TextStyle(
-                          color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold
-                          ),
-                          ),
-                        ),
-                    decoration: BoxDecoration(
-                    color: Colors.blue[800]
-                    )),
-                    Container(
-                      height: MediaQuery.of(context).size.height,
-                      child: ListView.builder(
-                      itemCount: 5,
-                      itemBuilder: (context, index){
-                            return SizedBox(
-                            height: 50,
-                            child: ListTile(
-                              leading: Icon(items[index].icon),
-                           title: Text(items[index].title),
-                               onTap: (){
-                               Navigator.push(context, MaterialPageRoute(builder: items[index].page));
-
-                       },
-                      ),
-                            );
-                      }),
-                    ),
-
-              ],
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(child:
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('Actas',
+              style: TextStyle(
+                  color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold
               ),
-              );
+            ),
+          ),
+              decoration: BoxDecoration(
+                  color: Colors.blue[800]
+              )),
+          ListTile(
+              title: GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => TabHomePage()));
+                  },
+                  child:  Row(
+                      children:[
+                        Icon(Icons.home),
+                        SizedBox(
+                            width: 10),
+                        Text('Home'),
+                      ]))),
+          ListTile(
+              title: GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => AppPage02()));
+                  },
+                  child:  Row(
+                      children:[
+                        Icon(Icons.favorite),
+                        SizedBox(
+                            width: 10),
+                        Text('고장 처리'),
+                      ]))),
+          ListTile(
+              title: GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => TabHomePage()));},
+                  child:  Row(
+                      children:[
+                        Icon(Icons.safety_check),
+                        SizedBox(
+                            width: 10),
+                        Text('현장 정보'),
+                      ]))),
+          ListTile(
+              title: GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => TabHomePage()));},
+                  child:  Row(
+                      children:[
+                        Icon(Icons.person_outlined),
+                        SizedBox(
+                            width: 10),
+                        Text('Account'),
+                      ]))),
+          ListTile(
+              title: GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => TabHomePage()));},
+                  child:  Row(
+                      children:[
+                        // SizedBox(height: MediaQuery.of(context).size.height/1.4,
+                        //     width: 190),
+                        Icon(Icons.door_back_door_outlined),
+                        SizedBox(
+                            width: 10),
+                        Text('Logout'),
+                      ]))),
+        ],
+      ),
+    );
   }
 
 
